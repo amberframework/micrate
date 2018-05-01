48c03493a8cb84545ed1ee1b3e79d4b0f948c263
Faustino Aguilar <faustinoaq@users.noreply.github.com>
2018-05-01 03:50:38 -0500

    Wait 1.millisecond before create migration file

--- src/micrate/cli.cr
+++ src/micrate/cli.cr
@@ -43,6 +43,7 @@ module Micrate
         raise "Migration name required"
       end
 
+      sleep 1.millisecond
       migration_file = Micrate.create(ARGV.shift, Time.now, migrations_path)
       puts "Created #{migration_file}"
     end
